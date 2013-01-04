f6e4c7dd7d8de1beb9f1bd46ce1b2694c722298a
Kim Merrill <kmerrill27@gmail.com>
2013-01-04 12:09:01 -0500

    finished

--- public/js/tenorion.js
+++ public/js/tenorion.js
@@ -6,46 +6,31 @@
  *
  **/
 var switchSize = 25, switchSpace = 5, switchOffset = 35, numSwitches = 16;
-var mouseX, mouseY;
-var xCoord, yCoord;
-var whiteCanvas, rippleCanvas, whiteCtx, rippleCtx;
-var canvasSize;
-var offColor = "#424559", onColor = "#A9AECC", playColor = "#EDEFFF", strokeColor = "#343854";
-var rippleColor = "rgba(237, 239, 255, 0.03)"
+var rippleOffset = 50;
 var tonePause = 150, ripplePause = 40;
 var dragging = false, turningOn = false;
-var rippleOffset = 50;
-var whiteSwitches = new Array(numSwitches);
-var currTones;
-var tonesDir = "tones/", tonesExt = ".wav";
-var lowTones = ["4C", "4B", "4A", "3G", "3F", "3E", "3D", "3C", "3B", "3A",
-	"2G", "2F", "2E", "2D", "2C", "2B"];
-var midTones = ["5C", "5B", "5A", "4G", "4F", "4E", "4D", "4C", "4B", "4A",
-	"3G", "3F", "3E", "3D", "3C", "3B"];
-var highTones = ["6C", "6B", "6A", "5G", "5F", "5E", "5D", "5C", "5B", "5A",
-	"4G", "4F", "4E", "4D", "4C", "4B"];
+var offColor = "#424559", onColor = "#A9AECC", playColor = "#EDEFFF";
+var strokeColor = "#343854", rippleColor = "rgba(237, 239, 255, 0.03)";
+var switches = new Array(numSwitches);
+var switchCanvas, rippleCanvas, switchCtx, rippleCtx;
+var canvasSize;
+var mouseX, mouseY;
 
 $(document).ready(function() {
-	whiteCanvas = $("#whiteCanvas");
-	whiteCtx = whiteCanvas[0].getContext("2d");
-	whiteCtx.strokeStyle = strokeColor;
-	canvasSize = whiteCanvas.width();
-	rippleCanvas = $("#rippleCanvas");
-	rippleCtx = rippleCanvas[0].getContext("2d");
-	rippleCtx.strokeStyle = rippleColor;
-	rippleCtx.lineWidth = 20;
+	// Set up switch canvas
+	switchCanvas = $("#switchCanvas");
+	switchCtx = switchCanvas[0].getContext("2d");
+	switchCtx.strokeStyle = strokeColor;
+	canvasSize = switchCanvas.width();
 
 	// Draw the Tenori-on
 	initBoard();
 
-	// Turn off all notes when the clear button is pressed
-	$("#clearButton").mousedown(function() {
-		for (var y = 0; y < numSwitches; y++) {
-			for (var x = 0; x < numSwitches; x++) {
-				turnOff(whiteSwitches[y][x]);
-			}
-		}
-	});
+	// Set up ripple canvas
+	rippleCanvas = $("#rippleCanvas");
+	rippleCtx = rippleCanvas[0].getContext("2d");
+	rippleCtx.strokeStyle = rippleColor;
+	rippleCtx.lineWidth = 20;
 
 	// If the mouse is pressed, toggle the current switch and set up
 	// the current drag
@@ -54,7 +39,7 @@ $(document).ready(function() {
 			coords = getSwitch(event.pageX, event.pageY);
 
 			if (coords != null) {
-				var currSwitch = whiteSwitches[coords[1]][coords[0]];
+				var currSwitch = switches[coords[1]][coords[0]];
 				turningOn = currSwitch.on;
 			}	
 
@@ -79,27 +64,40 @@ $(document).ready(function() {
 	rippleCanvas.mouseup(function(event) {
 		for (var y = 0; y < numSwitches; y++) {
 			for (var x = 0; x < numSwitches; x++) {
-				whiteSwitches[y][x].draggedOver = false;
+				switches[y][x].draggedOver = false;
+			}
+		}
+	});
+
+	// Turn off all notes when the clear button is pressed
+	$("#clearButton").mousedown(function() {
+		for (var y = 0; y < numSwitches; y++) {
+			for (var x = 0; x < numSwitches; x++) {
+				turnOff(switches[y][x]);
 			}
 		}
 	});
 });
 
+// Save x and y coordinates of mouse click
+function trackMouse(event) {
+	mouseX = event.pageX;
+	mouseY = event.pageY;
+};
+
 // Create Tenori-on, which is a 16x16 grid of LED switches
 function initBoard() {
+
 	// Load middle octave tones
-	for (var i = 0; i < numSwitches; i++) {
-		midTones[i] = new Audio(tonesDir + midTones[i] + tonesExt);
-	}
-	currTones = midTones;
+	initTones(numSwitches);
 
 	for (var y = 0; y < numSwitches; y++) {
-		whiteSwitches[y] = new Array(numSwitches);
+		switches[y] = new Array(numSwitches);
 		for (var x = 0; x < numSwitches; x++) {
 			xCoord = switchOffset + switchSize*x + switchSpace*x;
 			yCoord = switchOffset + switchSize*y + switchSpace*y;
 			drawSwitch(xCoord, yCoord, offColor);
-			whiteSwitches[y][x] = new ledSwitch(xCoord, yCoord);
+			switches[y][x] = new ledSwitch(xCoord, yCoord);
 		}
 	}
 
@@ -107,10 +105,37 @@ function initBoard() {
 	setInterval(function(){playTenorion()}, tonePause*numSwitches);
 
 	// Load other tones later so playback can start with middle octave tones
-	for (var i = 0; i < numSwitches; i++) {
-		lowTones[i] = new Audio(tonesDir + lowTones[i] + tonesExt);
-		highTones[i] = new Audio(tonesDir + highTones[i] + tonesExt);
+	loadAllTones(numSwitches);
+};
+
+// Create new LED switch
+function ledSwitch(xCoord, yCoord) {
+	this.x = xCoord;
+	this.y = yCoord;
+	this.on = false;
+	this.interval = null;
+	this.draggedOver = false;
+};
+
+// Draw a switch at the given location
+function drawSwitch(xCoord, yCoord, color, offset) {
+	// If no offset specified, set it to zero.
+	if (offset == null) {
+		offset = 0;
 	}
+	switchCtx.fillStyle = color;
+	switchCtx.beginPath();
+	switchCtx.clearRect(xCoord-1, yCoord-1, switchSize+2, switchSize+2);
+	switchCtx.arc(xCoord+switchSize/2, yCoord+switchSize/2, switchSize/2 + offset, 0,
+		Math.PI*2, true);
+	switchCtx.fill();
+	switchCtx.stroke();
+};
+
+// Turn on the given switch
+function turnOn(currSwitch) {
+	currSwitch.on = true;
+	drawSwitch(currSwitch.x, currSwitch.y, onColor);
 };
 
 // Turn off the given switch
@@ -120,12 +145,6 @@ function turnOff(currSwitch) {
 	setTimeout(function(){drawSwitch(currSwitch.x, currSwitch.y, offColor)}, 275);
 	clearInterval(currSwitch.interval);
 	currSwitch.interval = null;
-};
-
-// Turn on the given switch
-function turnOn(currSwitch) {
-	currSwitch.on = true;
-	drawSwitch(currSwitch.x, currSwitch.y, onColor);
 };
 
 // Turn the switch at the given coordinates on or off if appropriate
@@ -138,7 +157,7 @@ function toggle() {
 		row = coords[0];
 		col = coords[1];
 
-		var currSwitch = whiteSwitches[col][row];
+		var currSwitch = switches[col][row];
 
 		// Only toggle each switch once during a single drag
 		if (!currSwitch.draggedOver) {
@@ -152,30 +171,7 @@ function toggle() {
 		}
 
 		currSwitch.draggedOver = true;
-		whiteSwitches[col][row] = currSwitch;
-	}
-};
-
-// Play through the turned-on notes by iterating over the columns
-function playTenorion() {
-	var pause = 0;
-	for (var col = 0; col < numSwitches; col++) {
-		nextColumn(col, pause);
-		pause = pause + tonePause;
-	}
-};
-
-// Move to the next column to be played
-function nextColumn(col, pause) {
-	setTimeout(function(){playColumn(col)}, pause);
-};
-
-// Simultaneously play all turned-on switches in the given column
-function playColumn(col) {
-	for (var row = 0; row < numSwitches; row++) {
-		if (whiteSwitches[row][col].on) {
-			playSwitch(whiteSwitches[row][col], currTones[row]);
-		}
+		switches[col][row] = currSwitch;
 	}
 };
 
@@ -187,61 +183,28 @@ function playSwitch(currSwitch, tone) {
 	setTimeout(function(){drawSwitch(currSwitch.x, currSwitch.y, onColor)}, 250);
 };
 
-// Draw a switch at the given location
-function drawSwitch(xCoord, yCoord, color, offset) {
-	// If no offset specified, set it to zero.
-	if (offset == null) {
-		offset = 0;
+// Simultaneously play all turned-on switches in the given column
+function playColumn(col) {
+	for (var row = 0; row < numSwitches; row++) {
+		if (switches[row][col].on) {
+			playSwitch(switches[row][col], currTones[row]);
+		}
 	}
-	whiteCtx.fillStyle = color;
-	whiteCtx.beginPath();
-	whiteCtx.clearRect(xCoord-1, yCoord-1, switchSize+2, switchSize+2);
-	whiteCtx.arc(xCoord+switchSize/2, yCoord+switchSize/2, switchSize/2 + offset, 0,
-		Math.PI*2, true);
-	whiteCtx.fill();
-	whiteCtx.stroke();
 };
 
-// Save x and y coordinates of mouse click
-function trackMouse(event) {
-	mouseX = event.pageX;
-	mouseY = event.pageY;
+// Move to the next column to be played
+function nextColumn(col, pause) {
+	setTimeout(function(){playColumn(col)}, pause);
 };
 
-// Create new LED switch
-function ledSwitch(xCoord, yCoord) {
-	this.x = xCoord;
-	this.y = yCoord;
-	this.on = false;
-	this.interval = null;
-	this.draggedOver = false;
+// Play through the turned-on notes by iterating over the columns
+function playTenorion() {
+	var pause = 0;
+	for (var col = 0; col < numSwitches; col++) {
+		nextColumn(col, pause);
+		pause = pause + tonePause;
+	}
 };
-
-// Use mouse coordinates to determine on which switch the user clicked
-function getSwitch(xCoord, yCoord) {
-	// Calculate canvas element offset from top and left of window
-	var xOffset = whiteCanvas[0].offsetLeft + whiteCanvas[0].clientLeft +
-		whiteCanvas[0].scrollLeft;
-	var yOffset = whiteCanvas[0].offsetTop + whiteCanvas[0].clientTop +
-		whiteCanvas[0].scrollTop;
-
-	// Estimate of clicked switch's column and row in grid
-	xCoord = (xCoord - switchOffset - xOffset) / (switchSize + switchSpace);
-	yCoord = (yCoord - switchOffset - yOffset) / (switchSize + switchSpace);
-	var col = Math.floor(xCoord);
-	var row = Math.floor(yCoord);
-
-	// If user did not click on a switch - i.e. in the gridlines or on the outer
-	// border - return null
-	if (((xCoord - col) > .9) || ((yCoord - row) > .9) || 
-		row > (numSwitches-1) || col > (numSwitches-1) || row < 0 || col < 0) {
-		return null;
-	} 
-
-	// If user clicked on a switch, return the switch's column and row
-	return [col, row];
-};
-
 
 // Draw light rippling out from the given location
 function ripple(xCoord, yCoord, size) {
@@ -261,27 +224,27 @@ function ripple(xCoord, yCoord, size) {
  	}
  };
 
-// Make drag and drop permissible
-function allowDrop(event) {
-	event.preventDefault();
-};
+// Use mouse coordinates to determine on which switch the user clicked
+function getSwitch(xCoord, yCoord) {
+	// Calculate canvas element offset from top and left of window
+	var xOffset = switchCanvas[0].offsetLeft + switchCanvas[0].clientLeft +
+		switchCanvas[0].scrollLeft;
+	var yOffset = switchCanvas[0].offsetTop + switchCanvas[0].clientTop +
+		switchCanvas[0].scrollTop;
 
-// Save image to be dropped
-function drag(event) {
-	event.dataTransfer.setData("Image", event.target.id);
-};
+	// Estimate of clicked switch's column and row in grid
+	xCoord = (xCoord - switchOffset - xOffset) / (switchSize + switchSpace);
+	yCoord = (yCoord - switchOffset - yOffset) / (switchSize + switchSpace);
+	var col = Math.floor(xCoord);
+	var row = Math.floor(yCoord);
 
-// Move image to selected element
-function drop(event) {
- 	event.preventDefault();
- 	var treble = event.dataTransfer.getData("Image");
- 	event.target.appendChild($("#" + treble)[0]);
- 	// Change tones to selected octave 
- 	if (event.target.id == "highOct") {
- 		currTones = highTones;
- 	} else if (event.target.id == "midOct") {
- 		currTones = midTones;
- 	} else {
- 		currTones = lowTones;
- 	}
+	// If user did not click on a switch - i.e. in the gridlines or on the outer
+	// border - return null
+	if (((xCoord - col) > .9) || ((yCoord - row) > .9) || 
+		row > (numSwitches-1) || col > (numSwitches-1) || row < 0 || col < 0) {
+		return null;
+	} 
+
+	// If user clicked on a switch, return the switch's column and row
+	return [col, row];
 };
